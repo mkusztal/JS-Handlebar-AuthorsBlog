5a9d3c7b9cdf858ff9fc92d6fb4c089459786a15
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-02-09 23:15:45 +0200

    dodanie console.log

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const titleClickHandler = function (event) {
   console.log("Link was clicked!");
-
+  console.log(event);
   /* remove class 'active' from all article links  */
 
   /* add class 'active' to the clicked link */
