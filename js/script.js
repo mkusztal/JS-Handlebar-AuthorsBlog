945f584348cdaa3bd5d2f027f60de0c107555808
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-02-10 01:08:27 +0200

    Dodanie articleSelector

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
 const titleClickHandler = function (event) {
   console.log("Link was clicked!");
-  console.log(event);
+  event.preventDefault();
+  const clickedElement = this;
+
   /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll(".titles a.active");
 
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove("active");
+  }
   /* add class 'active' to the clicked link */
-
+  clickedElement.classList.add("active");
   /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll(".titles a.active");
 
+  for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove("active");
+  }
   /* get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute("href");
+  console.log(articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
